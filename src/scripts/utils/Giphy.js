825e31dae03730082a1452e6f6827fdbac65164b
Marie Eriksson <eriksson.km@gmail.com>
2017-02-14 14:51:56 +0100

    update giphy class

--- src/scripts/utils/Giphy.js
+++ src/scripts/utils/Giphy.js
@@ -1,9 +1,9 @@
 'use strict';
-
+import queryString from 'query-string';
 import { GIPHY_API_KEY } from '../config';
 export default class Giphy {
   constructor () {
-    this.search();
+    this.search('awesome');
   }
   /**
   * search for gif and return a promise.
@@ -12,11 +12,22 @@ export default class Giphy {
   *
   * @return {Promise}
   */
-  search(query) {
-    console.log("hej!");
-		const queryString = require('query-string');
-		let parsed = queryString.parse(query.search);
-		console.log(parsed);
-		//fetch(url + query + GIPHY_API_KEY);
-	}
+  search (q) {
+    const query = queryString.stringify({q});
+		fetch(`http://api.giphy.com/v1/gifs/search?${query}&api_key=${GIPHY_API_KEY}`)
+      .then(function (response) {
+        if (!response.ok) {
+          console.log('something went wrong. statuscode: ' + response.status);
+        } else {
+          response.json().then(function (json) {
+            let gifArray = [];
+            json.data.forEach(gif => gifArray.push(gif.images.fixed_width.url));
+            console.log(gifArray);
+          });
+        }
+      })
+      .catch(function (err) {
+        console.log('something went wrong.', err);
+      });
+  }
 }
